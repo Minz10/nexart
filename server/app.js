a04e8e7b518f1235dd40c8686668777852f4dd66
Minz69 <albert.minz01@gmail.com>
2020-10-05 04:12:15 +0530

    commited-auth-route

--- server/app.js
+++ server/app.js
@@ -1,8 +1,36 @@
 const express = require('express')
 const app = express()
+const mongoose = require('mongoose')
 const PORT = 5000
+const {MONGOURI} = require('./keys')
 
-const customMiddleware = (req,res,next)=>{ //syntax to use middleware
+//NaIVHnKmbRxbcWwk
+
+require('./models/user')
+
+app.use(express.json());
+app.use(require('./routes/auth'))
+
+
+mongoose.connect(MONGOURI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
+
+mongoose.connection.on('connected', ()=>{
+    console.log("Connected to Mongo");
+});
+
+mongoose.connection.on("error", (err) => {
+  console.log("Error connecting to Mongo");
+});
+
+app.listen(PORT,()=>{
+    console.log("Home")
+    console.log("Server is running on",PORT)
+})
+
+/*const customMiddleware = (req,res,next)=>{ //syntax to use middleware
     console.log("middleware executed!")
     next()
 }
@@ -16,8 +44,4 @@ app.get('/',(req,res)=>{
 app.get('/page2', customMiddleware,(req, res) => { //syntax for middleware to run for a specific route handler
     res.send("Welcome to Page 2")
 })
-
-app.listen(PORT,()=>{
-    console.log("Home")
-    console.log("Server is running on",PORT)
-})
+*/
---
+++ server/keys.js
@@ -0,0 +1,4 @@
+module.exports={
+    MONGOURI : "mongodb+srv://albert:NaIVHnKmbRxbcWwk@cluster0.kfuaj.mongodb.net/<dbname>?retryWrites=true&w=majority",
+    JWT_SECRET: "weffsdfgsdfrge"
+}
