c4c949d2bb74c90041bf9b1b9fce2a8dda10543e
johnmarandi <manshumarandi@gmail.com>
2020-10-06 14:24:37 +0530

    new change

--- client/src/components/screens/Home.js
+++ client/src/components/screens/Home.js
@@ -11,15 +11,9 @@ const Home = () => {
             Welcome to <br /> NeXart
           </h1>
           <p>A platform designed for your talent to be recognised.</p>
+
  
-const Home = ()=>{
-    return(
-        <h1 className='homeTemp'>
-            WELCOME TO NEXART!!!
-        </h1>
-    )
-}
- 
+
 
           <Link to="/Signup">
             <button className="btn"> Get Started </button>
