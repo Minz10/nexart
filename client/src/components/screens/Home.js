777d8524611eaba9c679a2f8b0d21bde7dd1cebe
johnmarandi <manshumarandi@gmail.com>
2020-10-07 11:16:02 +0530

    new change

--- client/src/components/screens/Home.js
+++ client/src/components/screens/Home.js
@@ -11,14 +11,10 @@ const Home = () => {
             Welcome to <br /> NeXart
           </h1>
           <p>A platform designed for your talent to be recognised.</p>
-<<<<<<< HEAD
 
  
 
 
-=======
-               
->>>>>>> 6d621b5195dcc46919c68c954abb110b3c542a98
           <Link to="/Signup">
             <button className="btn"> Get Started </button>
           </Link>
